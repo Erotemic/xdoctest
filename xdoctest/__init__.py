56aa9ea760a702053e4c0536afac74ebcc2643f0
joncrall <crallj@rpi.edu>
2018-05-27 20:42:40 -0400

    Changed development status to beta

--- xdoctest/__init__.py
+++ xdoctest/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 # flake8: noqa
-__version__ = '0.3.4'  # nocover
+__version__ = '0.3.5.dev0'  # nocover
 from xdoctest.exceptions import ExitTestException
 from xdoctest.runner import doctest_module  # nocover
