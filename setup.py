5813e7dba97a7193c67cf8ff52f7f1987249fad9
joncrall <crallj@rpi.edu>
2018-05-27 20:47:19 -0400

    converted README to rst

--- setup.py
+++ setup.py
@@ -100,6 +100,24 @@ def parse_requirements(fname='requirements.txt'):
     return []
 
 
+def parse_description():
+    """
+    Parse the description in the README file
+
+    CommandLine:
+        pandoc --from=markdown --to=rst --output=README.rst README.md
+        python -c "import setup; print(setup.parse_description())"
+    """
+    from os.path import dirname, join, exists
+    readme_fpath = join(dirname(__file__), 'README.rst')
+    # This breaks on pip install, so check that it exists.
+    if exists(readme_fpath):
+        with open(readme_fpath, 'r') as f:
+            text = f.read()
+        return text
+    return ''
+
+
 if __name__ == '__main__':
     setup(
         version=parse_version(),
@@ -108,7 +126,7 @@ if __name__ == '__main__':
         extras_require={
             'all': parse_requirements('optional-requirements.txt')
         },
-        # long_description=parse_description(),
+        long_description=parse_description(),
         entry_points={
             # the pytest11 entry point makes the plugin available to pytest
             'pytest11': [
