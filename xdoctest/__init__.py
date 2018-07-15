e33223e22caadfbebefe3286fba09ad8459cc25f
joncrall <erotemic@gmail.com>
2018-07-14 21:35:51 -0400

    wip

--- xdoctest/__init__.py
+++ xdoctest/__init__.py
@@ -2,9 +2,7 @@
 """
 mkinit xdoctest --nomods
 """
-__version__ = '0.5.1'  # nocover
-
-from xdoctest import docstr
+__version__ = '0.5.2'  # nocover
 
 # Expose only select submodules
 __submodules__ = [
@@ -18,4 +16,4 @@ from xdoctest.exceptions import (DoctestParseError, ExitTestException,
                                  MalformedDocstr,)
 
 __all__ = ['DoctestParseError', 'ExitTestException', 'MalformedDocstr',
-           'doctest_module', 'docstr']
+           'doctest_module']
