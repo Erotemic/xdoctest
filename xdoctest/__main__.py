34f00e7fd131c60899e92b41098ace0fd46c33dd
joncrall <jon.crall@kitware.com>
2018-08-13 08:42:37 -0400

    improve cmdline interface

--- xdoctest/__main__.py
+++ xdoctest/__main__.py
@@ -14,20 +14,44 @@ def main():
     python -m xdoctest networkx all --options=+IGNORE_WHITESPACE
     """
     import argparse
+    from os.path import exists
     from xdoctest import utils
-    description = utils.codeblock(
-        '''
-        discover and run doctests within a python package
-        ''')
 
-    parser = argparse.ArgumentParser(prog='python -m xdoctest', description=description)
-    parser.add_argument('modname', help='what files to run')
-    parser.add_argument('command', help='a doctest name or a command (list|all)', default='list')
+    parser = argparse.ArgumentParser(
+        prog='xdoctest',
+        description=utils.codeblock(
+            '''
+            discover and run doctests within a python package
+            ''')
+    )
+    # parser.add_argument('modname', help='what files to run')
+    # parser.add_argument('command', help='a doctest name or a command (list|all)', default='list')
+    parser.add_argument(
+        'arg', nargs='*', help=utils.codeblock(
+            '''
+            Ignored if optional arguments are specified, otherwise:
+            Defaults --modname to arg.pop(0).
+            Defaults --command to arg.pop(0).
+            '''))
+
+    # modname_or_path_group = parser.add_mutually_exclusive_group()
+    # modname_or_path_group.add_argument('modpath', help='module name or path')
+    parser.add_argument(*('-m', '--modname'), type=str,
+                        help='module name or path. If specified positional modules are ignored',
+                        default=None)
+
+    parser.add_argument(*('-c', '--command'), type=str,
+                        help='a doctest name or a command (list|all|<callname>). '
+                        'Defaults to all',
+                        default=None)
+
     parser.add_argument(*('--style',), type=str, help='choose your style',
                         choices=['auto', 'google', 'freeform'], default='auto')
+
     parser.add_argument(*('--options',), type=str,
                         help='specify the default directive state',
                         default=None)
+
     parser.add_argument(*('--offset',), dest='offset_linenos', action='store_true',
                         help=('Doctest outputs will display line numbers '
                               'wrt to the source file.'))
@@ -38,24 +62,53 @@ def main():
     # ... postprocess args
     modname = ns['modname']
     command = ns['command']
+    arg = ns['arg']
     style = ns['style']
     offset_linenos = ns['offset_linenos']
+    options = ns['options']
 
-    if ns['options'] is None:
-        from os.path import exists
-        ns['options'] = ''
+    # ---
+    # Allow for positional args to specify defaults for unspecified optionals
+    errors = []
+    if modname is None:
+        if len(arg) == 0:
+            errors += ['you must specify modname or modpath']
+        else:
+            modname = arg.pop(0)
+
+    if command is None:
+        if len(arg) == 0:
+            # errors += ['you must specify a command e.g (list, all)']
+            command = 'all'
+        else:
+            command = arg.pop(0)
+
+    if errors:
+        if len(errors) == 1:
+            errmsg = errors[0]
+        else:
+            listed_errors = ', '.join(['({}) {}'.format(c, e)
+                                       for c, e in enumerate(errors, start=1)])
+            errmsg = '{} errors: {}'.format(len(errors), listed_errors)
+        parser.error(errmsg)
+    # ---
+
+    from xdoctest.directive import parse_directive_optstr
+    import xdoctest
+
+    if options is None:
+        options = ''
         if exists('pytest.ini'):
             from six.moves import configparser
             parser = configparser.ConfigParser()
             parser.read('pytest.ini')
             try:
-                ns['options'] = parser.get('pytest', 'xdoctest_options')
+                options = parser.get('pytest', 'xdoctest_options')
             except configparser.NoOptionError:
                 pass
 
-    from xdoctest.directive import parse_directive_optstr
     default_runtime_state = {}
-    for optpart in ns['options'].split(','):
+    for optpart in options.split(','):
         if optpart:
             directive = parse_directive_optstr(optpart)
             if directive is not None:
@@ -67,7 +120,6 @@ def main():
         'offset_linenos': offset_linenos,
     }
 
-    import xdoctest
     xdoctest.doctest_module(modname, argv=[command], style=style,
                             config=config)
 
