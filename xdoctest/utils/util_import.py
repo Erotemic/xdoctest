4073e83b8dd543bf3b93f01f4719a89fa3a505b6
joncrall <erotemic@gmail.com>
2019-03-03 20:11:54 -0500

    pull and consolidate various updates to import code from ubelt

--- xdoctest/utils/util_import.py
+++ xdoctest/utils/util_import.py
@@ -1,19 +1,17 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function, division, absolute_import, unicode_literals
 import sys
-import importlib
 
 
 def import_module_from_name(modname):
     """
+    Imports a module from its string name (__name__)
+
     Args:
         modname (str):  module name
 
     Returns:
         module: module
-
-    CommandLine:
-        python -m xdoctest.utils import_module_from_name
 
     Example:
         >>> # test with modules that wont be imported in normal circumstances
@@ -32,6 +30,7 @@ def import_module_from_name(modname):
         # reproduce on another environment. Either way its better to use the
         # standard importlib implementation than the one I wrote a long time
         # ago.
+        import importlib
         module = importlib.import_module(modname)
     else:
         # The __import__ statment is weird
@@ -46,22 +45,116 @@ def import_module_from_name(modname):
 
 def import_module_from_path(modpath, index=-1):
     """
+    Imports a module via its path
+
     Args:
-        modpath (str): path to the module
+        modpath (PathLike): path to the module on disk or within a zipfile.
+
+    Returns:
+        module: the imported module
 
     References:
         https://stackoverflow.com/questions/67631/import-module-given-path
 
+    Notes:
+        If the module is part of a package, the package will be imported first.
+        These modules may cause problems when reloading via IPython magic
+
+        This can import a module from within a zipfile. To do this modpath
+        should specify the path to the zipfile and the path to the module
+        within that zipfile separated by a colon or pathsep.
+        E.g. `/path/to/archive.zip:mymodule.py`
+
+    Warning:
+        It is best to use this with paths that will not conflict with
+        previously existing modules.
+
+        If the modpath conflicts with a previously existing module name. And
+        the target module does imports of its own relative to this conflicting
+        path. In this case, the module that was loaded first will win.
+
+        For example if you try to import '/foo/bar/pkg/mod.py' from the folder
+        structure:
+          - foo/
+            +- bar/
+               +- pkg/
+                  +  __init__.py
+                  |- mod.py
+                  |- helper.py
+
+       If there exists another module named `pkg` already in sys.modules
+       and mod.py does something like `from . import helper`, Python will
+       assume helper belongs to the `pkg` module already in sys.modules.
+       This can cause a NameError or worse --- a incorrect helper module.
+
     Example:
-        >>> from xdoctest import utils
-        >>> modpath = utils.__file__.replace('.pyc', '.py')
+        >>> import xdoctest
+        >>> modpath = xdoctest.__file__
         >>> module = import_module_from_path(modpath)
-        >>> assert module is utils
+        >>> assert module is xdoctest
+
+    Example:
+        >>> # Test importing a module from within a zipfile
+        >>> import zipfile
+        >>> from xdoctest import utils
+        >>> from os.path import join, expanduser
+        >>> dpath = expanduser('~/.cache/xdoctest')
+        >>> dpath = utils.ensuredir(dpath)
+        >>> #dpath = utils.TempDir().ensure()
+        >>> # Write to an external module named bar
+        >>> external_modpath = join(dpath, 'bar.py')
+        >>> open(external_modpath, 'w').write('testvar = 1')
+        >>> internal = 'folder/bar.py'
+        >>> # Move the external bar module into a zipfile
+        >>> zippath = join(dpath, 'myzip.zip')
+        >>> with zipfile.ZipFile(zippath, 'w') as myzip:
+        >>>     myzip.write(external_modpath, internal)
+        >>> # Import the bar module from within the zipfile
+        >>> modpath = zippath + ':' + internal
+        >>> modpath = zippath + os.path.sep + internal
+        >>> module = import_module_from_path(modpath)
+        >>> assert module.__name__ == os.path.normpath('folder/bar')
+        >>> assert module.testvar == 1
+
+    Doctest:
+        >>> import pytest
+        >>> with pytest.raises(IOError):
+        >>>     import_module_from_path('does-not-exist')
+        >>> with pytest.raises(IOError):
+        >>>     import_module_from_path('does-not-exist.zip/')
     """
-    # the importlib version doesnt work in pytest
-    import xdoctest.static_analysis as static
-    dpath, rel_modpath = static.split_modpath(modpath)
-    modname = static.modpath_to_modname(modpath)
+    import os
+    if not os.path.exists(modpath):
+        import re
+        import zipimport
+        # We allow (if not prefer or force) the colon to be a path.sep in order
+        # to agree with the mod.__name__ attribute that will be produced
+
+        # zip followed by colon or slash
+        pat = '(.zip[' + re.escape(os.path.sep) + '/:])'
+        parts = re.split(pat, modpath, flags=re.IGNORECASE)
+        if len(parts) > 2:
+            archivepath = ''.join(parts[:-1])[:-1]
+            internal = parts[-1]
+            modname = os.path.splitext(internal)[0]
+            modname = os.path.normpath(modname)
+            if os.path.exists(archivepath):
+                zimp_file = zipimport.zipimporter(archivepath)
+                module = zimp_file.load_module(modname)
+                return module
+        raise IOError('modpath={} does not exist'.format(modpath))
+    else:
+        # the importlib version doesnt work in pytest
+        module = _custom_import_modpath(modpath)
+        # TODO: use this implementation once pytest fixes importlib
+        # module = _pkgutil_import_modpath(modpath)
+        return module
+
+
+def _custom_import_modpath(modpath, index=-1):
+    from xdoctest.static_analysis import split_modpath, modpath_to_modname
+    dpath, rel_modpath = split_modpath(modpath)
+    modname = modpath_to_modname(modpath)
     try:
         with PythonPathContext(dpath, index=index):
             module = import_module_from_name(modname)
@@ -69,19 +162,26 @@ def import_module_from_path(modpath, index=-1):
         print('Failed to import modname={} with modpath={}'.format(
             modname, modpath))
         raise
-    # TODO: use this implementation once pytest fixes importlib
-    # if six.PY2:  # nocover
-    #     import imp
-    #     module = imp.load_source(modname, modpath)
-    # elif sys.version_info[0:2] <= (3, 4):  # nocover
-    #     assert sys.version_info[0:2] <= (3, 2), '3.0 to 3.2 is not supported'
-    #     from importlib.machinery import SourceFileLoader
-    #     module = SourceFileLoader(modname, modpath).load_module()
-    # else:
-    #     import importlib.util
-    #     spec = importlib.util.spec_from_file_location(modname, modpath)
-    #     module = importlib.util.module_from_spec(spec)
-    #     spec.loader.exec_module(module)
+    return module
+
+
+def _pkgutil_import_modpath(modpath):  # nocover
+    import six
+    from xdoctest.static_analysis import split_modpath, modpath_to_modname
+    dpath, rel_modpath = split_modpath(modpath)
+    modname = modpath_to_modname(modpath)
+    if six.PY2:  # nocover
+        import imp
+        module = imp.load_source(modname, modpath)
+    elif sys.version_info[0:2] <= (3, 4):  # nocover
+        assert sys.version_info[0:2] <= (3, 2), '3.0 to 3.2 is not supported'
+        from importlib.machinery import SourceFileLoader
+        module = SourceFileLoader(modname, modpath).load_module()
+    else:
+        import importlib.util
+        spec = importlib.util.spec_from_file_location(modname, modpath)
+        module = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(module)
     return module
 
 
