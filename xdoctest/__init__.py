a32864070f533a2491863ef0749ca721b7e026a7
joncrall <erotemic@gmail.com>
2019-05-03 23:00:36 -0400

    finalize 0.8.0

--- xdoctest/__init__.py
+++ xdoctest/__init__.py
@@ -25,7 +25,7 @@ scripts), but for debugging it may be easier to use the native interface.
 
 """
 # mkinit xdoctest --nomods
-__version__ = '0.8.0.dev0'  # nocover
+__version__ = '0.8.0'  # nocover
 
 # Expose only select submodules
 __submodules__ = [
