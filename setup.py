ff91a4c961e00e96f3f2ee8ce917eeadf20781d6
joncrall <erotemic@gmail.com>
2019-07-05 20:22:28 -0400

    Shuffle base file structure. Improve requirements

--- setup.py
+++ setup.py
@@ -1,44 +1,11 @@
 # encoding: utf-8
 """
 Installation:
-    pip install git+https://github.com/Erotemic/xdoctest.git
+    pip install https://github.com/Erotemic/xdoctest/archive/master.zip
 
 Developing:
     git clone https://github.com/Erotemic/xdoctest.git
     pip install -e xdoctest
-
-Pypi:
-    cd ~/code/xdoctest
-
-    # Presetup
-    pip install twine
-
-    # First tag the source-code
-    VERSION=$(python -c "import setup; print(setup.parse_version())")
-    echo $VERSION
-    git tag $VERSION -m "tarball tag $VERSION"
-    git push --tags origin master
-
-    # NEW API TO UPLOAD TO PYPI
-    # https://packaging.python.org/tutorials/distributing-packages/
-
-    # Build wheel or source distribution
-    python setup.py bdist_wheel --universal
-
-    # Use twine to upload. This will prompt for username and password
-    twine upload --username erotemic --skip-existing dist/*
-
-    # Check the url to make sure everything worked
-    https://pypi.org/project/xdoctest/
-
-    # ---------- OLD ----------------
-    # Check the url to make sure everything worked
-    https://pypi.python.org/pypi?:action=display&name=xdoctest
-
-Notes:
-    In case you need to delete a tag
-    git push --delete origin tagname
-    git tag --delete tagname
 """
 from setuptools import setup
 # from setuptools import find_packages
@@ -123,9 +90,9 @@ if __name__ == '__main__':
     setup(
         version=parse_version(),
         description='A rewrite of the builtin doctest module',
-        install_requires=parse_requirements('requirements.txt'),
+        install_requires=parse_requirements('requirements/runtime.txt'),
         extras_require={
-            'all': parse_requirements('optional-requirements.txt')
+            'all': parse_requirements('requirements/optional.txt')
         },
         long_description=parse_description(),
         entry_points={
