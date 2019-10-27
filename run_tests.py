d7cba53016995c4f8f8f3cd351223c46202e7d9b
joncrall <erotemic@gmail.com>
2019-10-27 17:52:42 -0400

    cleanup

--- run_tests.py
+++ run_tests.py
@@ -2,16 +2,7 @@
 if __name__ == '__main__':
     import pytest
     import sys
-
-    # try:
-    #     import xdoctest
-    # except ImportError as ex:
-    #     print("FAIL XDOCTEST IMPORT")
-    #     print('ex = {!r}'.format(ex))
-    #     print("FAIL XDOCTEST IMPORT")
-    #     raise
-
-    # NOTE: import to return the correct error code
+    # NOTE: it is important to return the correct error code
     sys.exit(pytest.main([
         '-p', 'pytester',
         '-p', 'no:doctest',
@@ -21,12 +12,3 @@ if __name__ == '__main__':
         '--cov-report', 'term',
         '--xdoctest',
     ] + sys.argv[1:]))
-    # pytest.main(['-p', 'no:doctest', '-p', 'pytester', '--xdoctest', './testing'] + sys.argv[1:])
-    # pytest.main(['-p', 'no:doctest', '-p', 'pytester', '--xdoctest', './xdoctest'] + sys.argv[1:])
-    # pytest.main(['-p', 'no:doctest', '-p', 'pytester', '--xdoctest'] + sys.argv[1:])
-    # pytest.main(['-rsxX', '-p', 'pytester', 'xdoctest'] + sys.argv[1:])
-    # pytest.main(['-rsxX', '-p', 'pytester', 'xdoctest/tests'])
-
-#     pass
-# pytest -rxsX -p pytester xdoctest/tests
-# #pytest -rxsX -p pytester xdoctest/tests --xdoctest-modules
