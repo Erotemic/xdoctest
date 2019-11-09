24b5e76f60045adba588f5c524616f436e4f9ff3
joncrall <erotemic@gmail.com>
2019-11-08 21:18:39 -0500

    wip

--- publish.sh
+++ publish.sh
@@ -9,7 +9,9 @@ Args:
     USE_GPG : defaults to True
 
 Requirements:
-     twine
+     twine >= 1.13.0
+     gpg2 >= 2.2.4
+     OpenSSL >= 1.1.1c
 
 Notes:
     # NEW API TO UPLOAD TO PYPI
@@ -45,7 +47,7 @@ check_variable(){
 CURRENT_BRANCH=${CURRENT_BRANCH:=$(git branch | grep \* | cut -d ' ' -f2)}
 DEPLOY_BRANCH=${DEPLOY_BRANCH:=release}
 DEPLOY_REMOTE=${DEPLOY_REMOTE:=origin}
-VERSION=$(python -c "import setup; print(setup.parse_version())")
+VERSION=$(python -c "import setup; print(setup.version)")
 
 check_variable CURRENT_BRANCH
 check_variable DEPLOY_BRANCH
@@ -57,25 +59,31 @@ TWINE_USERNAME=${TWINE_USERNAME:=""}
 TWINE_PASSWORD=${TWINE_PASSWORD:=""}
 
 USE_GPG=${USE_GPG:="True"}
-GPG_EXECUTABLE=${GPG_EXECUTABLE:=gpg}
 
-__note___='''
-GPG_IDENTIFIER=Erotemic
-KEYID=$(gpg --list-keys --keyid-format LONG "$GPG_IDENTIFIER" | head -n 2 | tail -n 1 | awk '{print $1}' | tail -c 9)
-echo "KEYID = '$KEYID'"
+if [[ "$(which gpg2)" != "" ]]; then
+    GPG_EXECUTABLE=${GPG_EXECUTABLE:=gpg2}
+else
+    GPG_EXECUTABLE=${GPG_EXECUTABLE:=gpg}
+fi
 
-# https://help.github.com/en/articles/signing-commits
-git config --local commit.gpgsign true
-# Note the GPG key needs to match the email
-git config --local user.email $UserEmail
-# Tell git which key to sign
-git config --local user.signingkey $KEYID
-git config --local -l
+#__note___='''
+#GPG_IDENTIFIER=Erotemic
+#KEYID=$(gpg --list-keys --keyid-format LONG "$GPG_IDENTIFIER" | head -n 2 | tail -n 1 | awk '{print $1}' | tail -c 9)
+#echo "KEYID = '$KEYID'"
 
-GPG_KEY=297D757
-'''
+## https://help.github.com/en/articles/signing-commits
+#git config --local commit.gpgsign true
+## Note the GPG key needs to match the email
+#git config --local user.email $UserEmail
+## Tell git which key to sign
+#git config --local user.signingkey $KEYID
+#git config --local -l
+
+#GPG_KEYID=297D757
+#'''
 
 GPG_KEYID=${GPG_KEYID:=$(git config --local user.signingkey)}
+GPG_KEYID=${GPG_KEYID:=$(git config --global user.signingkey)}
 
 
 echo "
@@ -84,6 +92,7 @@ CURRENT_BRANCH='$CURRENT_BRANCH'
 DEPLOY_BRANCH='$DEPLOY_BRANCH'
 VERSION='$VERSION'
 TWINE_USERNAME='$TWINE_USERNAME'
+GPG_KEYID = '$GPG_KEYID'
 "
 
 
@@ -138,8 +147,8 @@ if [ "$USE_GPG" == "True" ]; then
     twine check $SDIST_PATH.asc $SDIST_PATH
 
     echo "Verifying wheels"
-    gpg --verify $BDIST_WHEEL_PATH.asc $BDIST_WHEEL_PATH 
-    gpg --verify $SDIST_PATH.asc $SDIST_PATH 
+    $GPG_EXECUTABLE --verify $BDIST_WHEEL_PATH.asc $BDIST_WHEEL_PATH 
+    $GPG_EXECUTABLE --verify $SDIST_PATH.asc $SDIST_PATH 
 
     check_variable BDIST_WHEEL_PATH
     check_variable SDIST_PATH
@@ -150,57 +159,57 @@ echo "
 === <END GPG SIGN> ===
 "
 
+if [[ "$CURRENT_BRANCH" != "$DEPLOY_BRANCH" ]]; then
+    TAG_AND_UPLOAD="no"
+    echo "current branch is not the deploy branch. Forcing tag_and_upload=no"
+fi
+
 
 # Verify that we want to publish
 if [[ "$TAG_AND_UPLOAD" != "yes" ]]; then
     if [[ "$TAG_AND_UPLOAD" != "no" ]]; then
-        read -p "Are you ready to publish version='$VERSION' on branch='$TRAVIS_BRANCH'? (input 'yes' to confirm)" ANS
+        read -p "Are you ready to publish version='$VERSION' on branch='$CURRENT_BRANCH'? (input 'yes' to confirm)" ANS
         echo "ANS = $ANS"
         TAG_AND_UPLOAD="$ANS"
     else
-        echo "Ready to publish VERSION='$VERSION' on branch='$TRAVIS_BRANCH'" 
+        echo "WRONG BRANCH: Not ready to publish VERSION='$VERSION' on branch='$CURRENT_BRANCH'" 
     fi
 else
-    echo "Not ready to publish VERSION='$VERSION' on branch='$TRAVIS_BRANCH'" 
+    echo "Do not want to publish VERSION='$VERSION' on branch='$CURRENT_BRANCH'" 
 fi
 
-if [[ "$TAG_AND_UPLOAD" == "yes" ]]; then
 
+if [[ "$TAG_AND_UPLOAD" == "yes" ]]; then
     check_variable TWINE_USERNAME
     check_variable TWINE_PASSWORD
 
-    if [[ "$CURRENT_BRANCH" == "$DEPLOY_BRANCH" ]]; then
-        echo "CURRENT_BRANCH = $CURRENT_BRANCH"
-        git tag $VERSION -m "tarball tag $VERSION"
-        git push --tags $DEPLOY_REMOTE $DEPLOY_BRANCH
-        if [ "$USE_GPG" == "True" ]; then
-            twine upload --username $TWINE_USERNAME --password=$TWINE_PASSWORD --sign $BDIST_WHEEL_PATH.asc $BDIST_WHEEL_PATH
-            twine upload --username $TWINE_USERNAME --password=$TWINE_PASSWORD --sign $SDIST_PATH.asc $SDIST_PATH
-        else
-            twine upload --username $TWINE_USERNAME --password=$TWINE_PASSWORD $BDIST_WHEEL_PATH 
-            twine upload --username $TWINE_USERNAME --password=$TWINE_PASSWORD $SDIST_PATH 
-        fi
-    else
-        echo "CURRENT_BRANCH!=DEPLOY_BRANCH. skipping tag and upload"
-        echo "ONLY ABLE TO PUBLISH ON DEPLOY CURRENT_BRANCH
+    git tag $VERSION -m "tarball tag $VERSION"
+    git push --tags $DEPLOY_REMOTE $DEPLOY_BRANCH
 
-        CURRENT_BRANCH = $CURRENT_BRANCH
-        DEPLOY_BRANCH = $DEPLOY_BRANCH
-        "
+    if [ "$USE_GPG" == "True" ]; then
+        twine upload --username $TWINE_USERNAME --password=$TWINE_PASSWORD --sign $BDIST_WHEEL_PATH.asc $BDIST_WHEEL_PATH
+        twine upload --username $TWINE_USERNAME --password=$TWINE_PASSWORD --sign $SDIST_PATH.asc $SDIST_PATH
+    else
+        twine upload --username $TWINE_USERNAME --password=$TWINE_PASSWORD $BDIST_WHEEL_PATH 
+        twine upload --username $TWINE_USERNAME --password=$TWINE_PASSWORD $SDIST_PATH 
     fi
-    echo " !!! FINISH: LIVE RUN !!!"
-else  
-    echo "... Skiping tag and upload"
-    echo " !!! FINISH: DRY RUN !!!"
-    echo "To do live run set TAG_AND_UPLOAD=yes"
+    echo """
+        !!! FINISH: LIVE RUN !!!
+    """
+else
+    ls dist
+    echo """
+        DRY RUN ... Skiping tag and upload
+
+        VERSION = '$VERSION'
+        DEPLOY_REMOTE = '$DEPLOY_REMOTE'
+        CURRENT_BRANCH = '$CURRENT_BRANCH'
+        DEPLOY_BRANCH = '$DEPLOY_BRANCH'
+        TAG_AND_UPLOAD = '$TAG_AND_UPLOAD'
+
+        To do live run set TAG_AND_UPLOAD=yes and ensure deploy and current branch are the same
+
+        !!! FINISH: DRY RUN !!!
+    """
 fi
 
-__notes__="""
-Notes:
-    # References: https://docs.travis-ci.com/user/deployment/pypi/
-
-    source $(load_secrets.sh)
-    travis encrypt TWINE_PASSWORD=$TWINE_PASSWORD  
-    travis encrypt TWINE_USERNAME=$TWINE_USERNAME 
-    source $(unload_secrets.sh)
-"""
