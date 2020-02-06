264ef4df769dddfb52947194a6795a79cef02d2a
joncrall <erotemic@gmail.com>
2020-02-06 00:23:50 -0500

    wip

--- docs/source/index.rst
+++ docs/source/index.rst
@@ -1,7 +1,5 @@
 :github_url: https://github.com/Erotemic/xdoctest
 
-.... automodule:: xdoctest.__init__
-..   :show-inheritance:
 
 .. The __init__ files contains the top-level documentation overview
 .. autoapimodule:: xdoctest
--- xdoctest/__init__.py
+++ xdoctest/__init__.py
@@ -24,7 +24,7 @@ Getting Started 0:  Installation
 You should have Python installed. Its also best practice to be in a `virtual
 environment <https://realpython.com/effective-python-environment/>`_. If you
 are a Python beginer and that doesn't make sense, then I would recommend
-setting up a `conda <https://docs.conda.io/en/latest/>`_.  environment. I
+setting up a `conda <https://docs.conda.io/en/latest/>`_ environment. I
 typically do this on Linux as follows:
 
 .. code:: bash
