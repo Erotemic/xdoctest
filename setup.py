0a877b29b5439fb894dab81507aaf62154f091a5
joncrall <erotemic@gmail.com>
2020-04-09 11:06:06 -0400

    Add 3.8 classifier

--- setup.py
+++ setup.py
@@ -217,6 +217,7 @@ if __name__ == '__main__':
             'License :: OSI Approved :: Apache Software License',
             # Supported Python versions
             'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.8',
             'Programming Language :: Python :: 3.7',
             'Programming Language :: Python :: 3.6',
             'Programming Language :: Python :: 3.5',
