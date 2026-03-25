84f5c1ee4ad9e8d01883030beaffc4ef9f1af2c8
joncrall <erotemic@gmail.com>
2026-03-25 17:35:02 -0400

    Fix and test async def linenos

--- tests/test_static.py
+++ tests/test_static.py
@@ -1,3 +1,4 @@
+import pytest
 from xdoctest import static_analysis as static
 from xdoctest import utils
 
@@ -149,6 +150,341 @@ def test_async_function_docstr_collection():
     assert 'b' in self.calldefs
     assert self.calldefs['b'].doclineno == 2
     assert self.calldefs['b'].doclineno_end == 5
+
+
+def test_parse_decorated_async_function_lineno():
+    source = utils.codeblock(
+        '''
+        def deco(func):
+            return func
+
+        @deco
+        async def foo():
+            """
+            Example:
+                >>> 1 + 1
+                2
+            """
+            return 42
+        '''
+    )
+
+    modpath = 'test_parse_decorated_async_function_lineno.py'
+    calldefs = static.parse_static_calldefs(
+        source=source,
+        fpath=modpath,
+    )
+
+    assert 'foo' in calldefs
+    calldef = calldefs['foo']
+
+    # Should point to the `async def foo():` line, not the decorator line.
+    source_lines = source.splitlines()
+    assert source_lines[calldef.lineno - 1].strip() == 'async def foo():'
+
+    # And it should still extract the docstring normally.
+    assert '>>> 1 + 1' in calldef.docstr
+
+
+def test_parse_multi_decorated_async_function_lineno():
+    source = utils.codeblock(
+        '''
+        def deco1(func):
+            return func
+
+        def deco2(func):
+            return func
+
+        @deco1
+        @deco2
+        async def foo():
+            """
+            Example:
+                >>> 1 + 1
+                2
+            """
+            return 42
+        '''
+    )
+
+    calldefs = static.parse_static_calldefs(
+        source=source,
+        fpath='dummy.py',
+    )
+    calldef = calldefs['foo']
+
+    source_lines = source.splitlines()
+    assert source_lines[calldef.lineno - 1].strip() == 'async def foo():'
+    assert '>>> 1 + 1' in calldef.docstr
+
+
+@pytest.mark.parametrize('case', [
+    {
+        'name': 'decorated_async_single',
+        'source': '''
+            def deco(func):
+                return func
+
+            @deco
+            async def foo():
+                """
+                Example:
+                    >>> 1 + 1
+                    2
+                """
+                return 42
+        ''',
+        'callname': 'foo',
+        'expect_line': 'async def foo():',
+    },
+    {
+        'name': 'decorated_async_multiple',
+        'source': '''
+            def deco1(func):
+                return func
+
+            def deco2(func):
+                return func
+
+            @deco1
+            @deco2
+            async def foo():
+                """
+                Example:
+                    >>> 1 + 1
+                    2
+                """
+                return 42
+        ''',
+        'callname': 'foo',
+        'expect_line': 'async def foo():',
+    },
+    {
+        'name': 'decorated_async_with_args',
+        'source': '''
+            def deco(func):
+                return func
+
+            def deco_factory(*args, **kwargs):
+                def wrap(func):
+                    return func
+                return wrap
+
+            @deco_factory(arg=1)
+            async def foo():
+                """
+                Example:
+                    >>> 1 + 1
+                    2
+                """
+                return 42
+        ''',
+        'callname': 'foo',
+        'expect_line': 'async def foo():',
+    },
+    {
+        'name': 'decorated_async_mixed',
+        'source': '''
+            def deco1(func):
+                return func
+
+            def deco2(*args, **kwargs):
+                def wrap(func):
+                    return func
+                return wrap
+
+            @deco1
+            @deco2(arg=1)
+            async def foo():
+                """
+                Example:
+                    >>> 1 + 1
+                    2
+                """
+                return 42
+        ''',
+        'callname': 'foo',
+        'expect_line': 'async def foo():',
+    },
+    {
+        'name': 'decorated_async_with_comment',
+        'source': '''
+            def deco1(func):
+                return func
+
+            def deco2(func):
+                return func
+
+            @deco1
+            # comment between decorators
+            @deco2
+            async def foo():
+                """
+                Example:
+                    >>> 1 + 1
+                    2
+                """
+                return 42
+        ''',
+        'callname': 'foo',
+        'expect_line': 'async def foo():',
+    },
+    {
+        'name': 'decorated_async_multiline_signature',
+        'source': '''
+            def deco(func):
+                return func
+
+            @deco
+            async def foo(
+                    a, b,
+                    c):
+                """
+                Example:
+                    >>> 1 + 1
+                    2
+                """
+                return 42
+        ''',
+        'callname': 'foo',
+        'expect_line': 'async def foo(',
+    },
+    {
+        'name': 'decorated_sync_and_async_together',
+        'source': '''
+            def deco(func):
+                return func
+
+            @deco
+            def bar():
+                """
+                Example:
+                    >>> "bar"
+                    'bar'
+                """
+                return 'bar'
+
+            @deco
+            async def foo():
+                """
+                Example:
+                    >>> 1 + 1
+                    2
+                """
+                return 42
+        ''',
+        'callname': 'foo',
+        'expect_line': 'async def foo():',
+    },
+    {
+        'name': 'decorated_method_in_class',
+        'source': '''
+            def deco(func):
+                return func
+
+            class C:
+                @classmethod
+                @deco
+                async def foo(cls):
+                    """
+                    Example:
+                        >>> 1 + 1
+                        2
+                    """
+                    return 42
+        ''',
+        'callname': 'C.foo',
+        'expect_line': 'async def foo(cls):',
+    },
+    {
+        'name': 'decorated_staticmethod_in_class',
+        'source': '''
+            def deco(func):
+                return func
+
+            class C:
+                @staticmethod
+                @deco
+                async def foo():
+                    """
+                    Example:
+                        >>> 1 + 1
+                        2
+                    """
+                    return 42
+        ''',
+        'callname': 'C.foo',
+        'expect_line': 'async def foo():',
+    },
+    {
+        'name': 'decorated_async_after_module_docstring_and_blanks',
+        'source': '''
+            """
+            module docstring
+            """
+
+
+            def deco(func):
+                return func
+
+
+            @deco
+            async def foo():
+                """
+                Example:
+                    >>> 1 + 1
+                    2
+                """
+                return 42
+        ''',
+        'callname': 'foo',
+        'expect_line': 'async def foo():',
+    },
+    {
+        'name': 'decorated_async_non_ascii_prefix',
+        'source': '''
+            # café
+
+            def deco(func):
+                return func
+
+            @deco
+            async def foo():
+                """
+                Example:
+                    >>> 1 + 1
+                    2
+                """
+                return 42
+        ''',
+        'callname': 'foo',
+        'expect_line': 'async def foo():',
+    },
+])
+def test_parse_decorated_function_lineno_cases(case):
+    source = utils.codeblock(case['source'])
+
+    calldefs = static.parse_static_calldefs(
+        source=source,
+        fpath=case['name'] + '.py',
+    )
+
+    assert case['callname'] in calldefs, (
+        'Missing calldef {!r} in case {!r}. Got keys={!r}'.format(
+            case['callname'], case['name'], sorted(calldefs.keys()))
+    )
+
+    calldef = calldefs[case['callname']]
+    source_lines = source.splitlines()
+
+    got_line = source_lines[calldef.lineno - 1].strip()
+    assert got_line == case['expect_line'], (
+        'Wrong lineno for case={!r}. Expected line={!r}, got line={!r}, '
+        'lineno={!r}'.format(
+            case['name'], case['expect_line'], got_line, calldef.lineno)
+    )
+
+    assert '>>> 1 + 1' in calldef.docstr or ">>> \"bar\"" in calldef.docstr
 
 
 if __name__ == '__main__':
